1d99143c616e82aad5c68e55c419b165a1b5cf74
Carly Petracco <carly.k.p@gmail.com>
2018-02-23 13:50:27 +0000

    Cocktails with photos

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -30,6 +30,6 @@ private
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def cocktail_params
-      params.require(:cocktail).permit(:name)
+      params.require(:cocktail).permit(:name, :photo)
     end
 end
